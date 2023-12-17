01bcdb5acdd988b6dd55bef0da3d2dd4cc0dadb5
Seleem <sseleem1601@gmail.com>
2023-12-17 14:11:08 -0500

    X

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,12 @@ import DefaultLayout from "@/layouts/default";
 export default function IndexPage() {
     return (
         <DefaultLayout>
-            <div className="container mx-auto mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="container mx-auto mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {[...Array(3)].map((_, index) => (
-                    <Card key={index} className="bg-transparent">
-                        <CardHeader className="flex-col items-start">
-                            <p className="text-tiny uppercase font-bold">Category {index + 1}</p>
-                            <h4 className="font-bold text-large">Card Title {index + 1}</h4>
-                        </CardHeader>
+                    <Card key={index} className="bg-transparent" isFooterBlurred
+					radius="lg"
+					className="border-none">
+                        
                         <Image
                             src={`./images/card${index + 1}.jpeg`} // Replace with your image paths
                             alt={`Card Image ${index + 1}`}
@@ -19,6 +18,7 @@ export default function IndexPage() {
                             width="100%"
                             height="auto"
                         />
+						
                     </Card>
                 ))}
             </div>
