263c96df1d52b75057243cc9c8955f04082512aa
Seleem <sseleem1601@gmail.com>
2023-12-18 23:27:40 -0500

    Updating component

--- components/productioncard.tsx
+++ components/productioncard.tsx
@@ -1,35 +1,34 @@
 import React from "react";
-import {Card, CardBody, Image, Button, Slider} from "@nextui-org/react";
+import { Card, CardBody, Image, Button, Slider } from "@nextui-org/react";
 import { FaYoutube, FaSpotify, FaApple } from 'react-icons/fa';
 
-
-export default function productioncard() {
+export default function ProductionCard({ title, description, imageSrc, youtubeLink, spotifyLink, appleLink }) {
   const [liked, setLiked] = React.useState(false);
 
   return (
     <Card
       isBlurred
-      className="border-none bg-background/60 dark:bg-default-100/50 w-full md:max-w-[1200px]"
+      className="border-none bg-background/60 dark:bg-default-100/50 w-full mb-5md:max-w-[1200px]"
       shadow="sm"
     >
       <CardBody>
         <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
-          <div className="relative col-span-6 md:col-span-4">
+          <div className="relative col-span-6 md:col-span-6">
             <Image
-              alt="Album cover"
+              alt={title}
               className="object-cover"
               height={200}
               shadow="md"
-              src="/images/card1.jpeg"
+              src={imageSrc}
               width="100%"
             />
           </div>
 
-          <div className="flex flex-col col-span-6 md:col-span-8">
+          <div className="flex flex-col col-span-6 md:col-span-6">
             <div className="flex justify-between items-start">
               <div className="flex flex-col gap-0 text-right">
-                <h1 className="font-semibold text-foreground/90">اشتري مني</h1>
-                <p className="text-small text-foreground/80">إحنا ٣ أصحاب بنتكلم من ٣ وجهات نظر مختلفة، قعدتنا مفيهاش استفادة لو مش بتعتبر الانبساط ده استفادة. من إنتاج ذا بوتكاست برودكشنز.</p>
+                <h1 className="font-semibold text-foreground/90 text-3xl">{title}</h1>
+                <p className="text-small text-foreground/80">{description}</p>
               </div>
               <Button
                 isIconOnly
@@ -41,18 +40,17 @@ export default function productioncard() {
               </Button>
             </div>
 
-
             <div className="mt-4 flex justify-center gap-2">
-                            <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-red-600 text-white rounded-full flex items-center justify-center gap-2">
-                                <FaYoutube size="30" /> 
-                            </a>
-                            <a href="https://open.spotify.com/show/2GcpoqDhqssSxDeugg4ejF?si=y6UEsDGZSZWHrCc0fpmezQ&utm_medium=share&utm_source=linktree&nd=1&dlsi=afa9dae822884a7f" target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-green-600 text-white rounded-full flex items-center justify-center gap-2 md:text-small">
-                                <FaSpotify size="30"/> 
-                            </a>
-                            <a href="https://podcasts.apple.com/eg/podcast/eshtry-menny-%D8%A7%D8%B4%D8%AA%D8%B1%D9%8A-%D9%85%D9%86%D9%8A/id1625454150" target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-gray-800 text-white rounded-full flex items-center justify-center gap-2">
-                                <FaApple size="30"/> 
-                            </a>
-                        </div>
+              <a href={youtubeLink} target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-red-600 text-white rounded-full flex items-center justify-center gap-2">
+                <FaYoutube size="30" />
+              </a>
+              <a href={spotifyLink} target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-green-600 text-white rounded-full flex items-center justify-center gap-2 md:text-small">
+                <FaSpotify size="30"/>
+              </a>
+              <a href={appleLink} target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-gray-800 text-white rounded-full flex items-center justify-center gap-2">
+                <FaApple size="30"/>
+              </a>
+            </div>
           </div>
         </div>
       </CardBody>
