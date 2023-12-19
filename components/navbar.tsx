f0a19a85e0a6d5c017ff8922f70671d452bc2818
Seleem <sseleem1601@gmail.com>
2023-12-19 12:22:40 -0500

    Removed previous changes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -44,22 +44,7 @@ export const Navbar = () => {
 
 
 	return (
-		<NextUINavbar onMenuOpenChange={setIsMenuOpen} classNames={{
-			item: [
-			  "flex",
-			  "relative",
-			  "h-full",
-			  "items-center",
-			  "data-[active=true]:after:content-['']",
-			  "data-[active=true]:after:absolute",
-			  "data-[active=true]:after:bottom-0",
-			  "data-[active=true]:after:left-0",
-			  "data-[active=true]:after:right-0",
-			  "data-[active=true]:after:h-[2px]",
-			  "data-[active=true]:after:rounded-[2px]",
-			  "data-[active=true]:after:bg-primary",
-			],
-		  }} maxWidth="xl" position="sticky">
+		<NextUINavbar onMenuOpenChange={setIsMenuOpen} className ="mt-4 mb-5 " maxWidth="xl" position="sticky">
 			<NavbarContent className="flex justify-between items-center  mt-10 mb-10 w-full">
 			
 				
