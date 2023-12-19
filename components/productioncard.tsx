26d7dda45f4eb21dba09f617addc565dcf138df5
Seleem <sseleem1601@gmail.com>
2023-12-19 09:35:07 -0500

    Updating productioncards

--- components/productioncard.tsx
+++ components/productioncard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, CardBody, Image, Button, Slider } from "@nextui-org/react";
 import { FaYoutube, FaSpotify, FaApple } from 'react-icons/fa';
 
-export default function ProductionCard({ title, description, imageSrc, youtubeLink, spotifyLink, appleLink, anghamiLink }) {
+export default function ProductionCard({ title, description, imageSrc, youtubeLink, youtubeEmbed, spotifyLink, appleLink, anghamiLink }) {
   const [liked, setLiked] = React.useState(false);
 
   return (
@@ -13,7 +13,7 @@ export default function ProductionCard({ title, description, imageSrc, youtubeLi
     >
       <CardBody>
         <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
-          <div className="relative col-span-6 md:col-span-6">
+          <div className="relative col-span-6 md:col-span-5">
             <Image
               alt={title}
               className="object-cover"
@@ -24,12 +24,25 @@ export default function ProductionCard({ title, description, imageSrc, youtubeLi
             />
           </div>
 
-          <div className="flex flex-col col-span-6 md:col-span-6">
+          <div className="flex flex-col col-span-6 md:col-span-7">
             <div className="flex justify-between items-start">
-              <div className="flex flex-col gap-0 text-right">
+              <div className="flex flex-col gap-0  mt-2 text-right">
                 <h1 className="font-semibold text-foreground/90 text-3xl mb-2 md:text-5xl">{title}</h1>
                 <p className="text-small md:text-xl text-foreground/80">{description}</p>
+
+                <div className="hidden md:flex justify-center items-center mt-4">
+                  <iframe 
+                    width="560" 
+                    height="315" 
+                    src={youtubeEmbed}
+                    title="YouTube video player" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen>
+                  </iframe>
+                </div>
               </div>
+              
               <Button
                 isIconOnly
                 className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
