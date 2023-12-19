e3de95617e68b07d7a116bf6e5bf3a6ece675d02
Seleem <sseleem1601@gmail.com>
2023-12-19 18:14:37 -0500

    Update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -75,20 +75,20 @@ export const Navbar = () => {
 			className="sm:hidden"
 			/>
 
-      <NavbarMenu className=" shadow-lg rounded-lg p-4">
-        <div className="flex flex-col gap-3 mt-5">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Button
-				onClick={() => closeMenuAndNavigate(item.href)} // Handle navigation here
-                className="w-full text-xl py-4"
-              >
-                {item.label}
-              </Button>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
+			<NavbarMenu className="bg-white/30 backdrop-blur-md shadow-lg rounded-lg p-4">
+					<div className="flex flex-col bg-white/30 backdrop-blur-md gap-3 mt-5">
+					{siteConfig.navMenuItems.map((item, index) => (
+						<NavbarMenuItem key={`${item}-${index}`}>
+						<Button
+							onClick={() => closeMenuAndNavigate(item.href)}
+							className="w-full text-xl py-4 bg-white/20 hover:bg-white/30 text-gray-800 font-medium transition duration-300 ease-in-out shadow-md rounded-lg"
+						>
+							{item.label}
+						</Button>
+						</NavbarMenuItem>
+					))}
+					</div>
+				</NavbarMenu>
 		</NextUINavbar>
 	);
 };
