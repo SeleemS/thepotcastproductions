b5defb287f5fa51d56d601de976f13039bc37151
Seleem <sseleem1601@gmail.com>
2023-12-19 12:21:54 -0500

    Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -44,19 +44,37 @@ export const Navbar = () => {
 
 
 	return (
-		<NextUINavbar onMenuOpenChange={setIsMenuOpen} className ="mt-4 mb-5" maxWidth="xl" position="sticky">
-			<NavbarContent className="flex justify-between items-center mt-5 mb-5 w-full height-200">
+		<NextUINavbar onMenuOpenChange={setIsMenuOpen} classNames={{
+			item: [
+			  "flex",
+			  "relative",
+			  "h-full",
+			  "items-center",
+			  "data-[active=true]:after:content-['']",
+			  "data-[active=true]:after:absolute",
+			  "data-[active=true]:after:bottom-0",
+			  "data-[active=true]:after:left-0",
+			  "data-[active=true]:after:right-0",
+			  "data-[active=true]:after:h-[2px]",
+			  "data-[active=true]:after:rounded-[2px]",
+			  "data-[active=true]:after:bg-primary",
+			],
+		  }} maxWidth="xl" position="sticky">
+			<NavbarContent className="flex justify-between items-center  mt-10 mb-10 w-full">
 			
 				
-                <NavbarBrand className="max-w-fit">
+				<NavbarBrand className="max-w-fit">
                     <NextLink href="/">
-                        <img src="/images/logo3.png" alt="Logo" width="125" height="36" />
+                        {/* Adjust the size of the logo image */}
+                        <img src="/images/logo3.png" alt="Logo" width="100" height="36" />
                     </NextLink>
                 </NavbarBrand>
 				{/*<ThemeSwitch/>*/}
+
 				
 
                 <div className="hidden lg:flex gap-4 ml-auto"> {/* Note the addition of ml-auto */}
+				
                     {siteConfig.navItems.map((item) => (
                         <NavbarItem key={item.href}>
                             <NextLink
