23e9096d563edcd8d6b92ce8e22d6a55f43495b6
Seleem <sseleem1601@gmail.com>
2023-12-19 18:16:23 -0500

    Update margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,7 +72,7 @@ export default function IndexPage() {
                             />
                         </div>
                         <div className="p-8 flex flex-col justify-center md:w-1/2">
-                            <h3 className={`${title({color:"green"})} text-3xl font-semibold mb-4`}>White Label Solutions</h3>
+                            <h3 className={`${title({color:"green"})} text-3xl font-semibold mt-[-15px] mb-4`}>White Label Solutions</h3>
                             <h4 className={`${features({color:"yellow"})} text-lg mb-2`}>Podcast Editing</h4>
                             <h4 className={`${features({color:"blue"})} text-lg mb-2`}>Podcast Hosting</h4>
                             <h4 className={`${features({color:"cyan"})} text-lg mb-2`}>Podcast Analytics</h4>
