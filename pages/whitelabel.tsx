41c442e430c44819aec8af43edc30227b12a96e4
Seleem <sseleem1601@gmail.com>
2023-12-19 19:18:05 -0500

    Updating content

--- pages/whitelabel.tsx
+++ pages/whitelabel.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, Image } from "@nextui-org/react";
 import DefaultLayout from "@/layouts/default";
-import { title } from "@/components/primitives";
+import { title, features } from "@/components/primitives";
 
 const WhiteLabelPage = () => {
     return (
@@ -15,8 +15,9 @@ const WhiteLabelPage = () => {
                             alt="Turnkey Designs"
                             className="md:w-4/5 w-full"
                         />
-                        <div className="flex items-center p-4 md:w-1/5 w-full">
+                        <div className="flex flex-col items-center p-4 md:w-1/5 w-full">
                             <h3 className={`${title()} text-lg md:text-xl`}>Turnkey Designs</h3>
+                            <h4 className={`${features({color:"yellow"})} text-lg mb-2`}>Bespoke podcast content and state-of-the-art studio design.</h4>
                         </div>
                     </Card>
 
@@ -26,8 +27,9 @@ const WhiteLabelPage = () => {
                             alt="Scalable Solutions"
                             className="md:w-4/5 w-full"
                         />
-                        <div className="flex items-center p-4 md:w-1/5 w-full">
+                        <div className="flex flex-col items-center p-4 md:w-1/5 w-full">
                             <h3 className={`${title()} text-lg md:text-xl`}>Podcast Production</h3>
+                            <h4 className={`${features({color:"blue"})} text-lg mb-2`}>Expert Hosting, Advanced Sound & Video Editing, and Creative Scriptwriting.</h4>
                         </div>
                     </Card>
 
@@ -37,8 +39,9 @@ const WhiteLabelPage = () => {
                             alt="Reliable Support"
                             className="md:w-4/5 w-full"
                         />
-                        <div className="flex items-center p-4 md:w-1/5 w-full">
+                        <div className="flex flex-col items-center p-4 md:w-1/5 w-full">
                             <h3 className={`${title()} text-lg md:text-xl`}>Podcast Analytics</h3>
+                            <h4 className={`${features({color:"cyan"})} text-lg mb-2`}>Streamlined Tools for Enhanced Audience Engagement</h4>
                         </div>
                     </Card>
                 </div>
