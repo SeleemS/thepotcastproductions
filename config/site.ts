ab3a4d1058a7a9d17159310eb2190b808edbcdce
Seleem <sseleem1601@gmail.com>
2023-12-20 23:30:16 -0500

    Update links

--- config/site.ts
+++ config/site.ts
@@ -42,8 +42,5 @@ export const siteConfig = {
 	links: {
 		github: "https://github.com/nextui-org/nextui",
 		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui-docs-v2.vercel.app",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
 	},
 };
