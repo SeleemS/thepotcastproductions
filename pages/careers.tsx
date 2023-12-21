6032e492dec95c17c7cdfa15f421ada71454ac88
Seleem <sseleem1601@gmail.com>
2023-12-21 17:48:57 -0500

    Redirect straight to jobs table

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -3,7 +3,7 @@ import { GetServerSideProps } from 'next';
 export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     redirect: {
-      destination: 'https://potcast-careers.notion.site/Job-Board-c0c376a3672e4b2591d57c134a8e96de',
+      destination: 'https://www.notion.so/potcast-careers/33bcf74995e242b3a2456b2f58f1a184?v=4832f153258e413bb5823583d9bca47d',
       permanent: false, // set to true if this is a permanent redirect
     },
   };
