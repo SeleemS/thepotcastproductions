538fb9f43b40669b0a372585dd7488e875aab4a7
Seleem <sseleem1601@gmail.com>
2023-12-21 12:52:35 -0500

    Link form to Formspree

--- pages/contactus.tsx
+++ pages/contactus.tsx
@@ -17,51 +17,61 @@ export default function ContactUsPage() {
                 <Card className="bg-white/90 backdrop-blur-md border border-gray-200 w-full p-4 " isBlurred radius="lg">
                     <h2 className="text-2xl font-semibold mb-4 text-center">Contact Us</h2>
                     
-                    <form className="flex flex-col gap-4">
+                    <form 
+                        action="https://formspree.io/f/mrgnzyak" 
+                        method="POST" 
+                        className="flex flex-col gap-4"
+                    >
                         <Input
-                            isRequired
-                            type="name"
-                            label="Name"
-                            className="max-w-full border-2 border-green-800 rounded-lg"
-                            />
-                         <Input
-                            isRequired
-                            type="email"
-                            label="Email"
-                            className="max-w-full border-2 border-green-800 rounded-lg"
-                            />
-                          <Input
-                            type="tel"
-                            label="Phone Number"
-                            className="max-w-full border-2 border-green-800 rounded-lg"
+                        isRequired
+                        type="name"
+                        name="name"  // Add a name attribute
+                        label="Name"
+                        className="max-w-full border-2 border-green-800 rounded-lg"
+                        />
+                        <Input
+                        isRequired
+                        type="email"
+                        name="email"  // Add a name attribute
+                        label="Email"
+                        className="max-w-full border-2 border-green-800 rounded-lg"
+                        />
+                        <Input
+                        type="tel"
+                        name="phone"  // Add a name attribute
+                        label="Phone Number"
+                        className="max-w-full border-2 border-green-800 rounded-lg"
                         />
 
                         {/* Dropdown for Subject */}
                         <Select
-                            isRequired
-                            label="Subject"
-                            className="max-w-full border-2 border-green-800 rounded-lg"
+                        isRequired
+                        name="subject"  // Add a name attribute
+                        label="Subject"
+                        className="max-w-full border-2 border-green-800 rounded-lg"
                         >
-                            {subjects.map((subject) => (
-                                <SelectItem key={subject.value} value={subject.value}>
-                                    {subject.label}
-                                </SelectItem>
-                            ))}
+                        {subjects.map((subject) => (
+                            <SelectItem key={subject.value} value={subject.value}>
+                            {subject.label}
+                            </SelectItem>
+                        ))}
                         </Select>
-                        
+
                         <Textarea
-                            isRequired
-                            label="Message"
-                            placeholder="How can we help?"
-                            className="max-w-full border-2 border-green-800 rounded-lg mb-[-7px]"
-                            rows={3}
+                        isRequired
+                        name="message"  // Add a name attribute
+                        label="Message"
+                        placeholder="How can we help?"
+                        className="max-w-full border-2 border-green-800 rounded-lg mb-[-7px]"
+                        rows={3}
                         />
                         <Button
-                            size="lg"
-                            className="bg-gradient-to-tr from-[#03a07d] to-[#006400] hover:from-[#029d70] hover:to-[#005700] text-white shadow-lg transition-all duration-300 ease-in-out"
+                        type="submit"  // Change to submit type
+                        size="lg"
+                        className="bg-gradient-to-tr from-[#03a07d] to-[#006400] hover:from-[#029d70] hover:to-[#005700] text-white shadow-lg transition-all duration-300 ease-in-out"
                         >
-                            Send Message
-                            <FaPaperPlane className="ml-2" />
+                        Send Message
+                        <FaPaperPlane className="ml-2" />
                         </Button>
                     </form>
                 </Card>
