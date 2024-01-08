93cbded10c8f9109fa1e8119bc91a26e424d2943
Seleem <sseleem1601@gmail.com>
2024-01-08 12:27:41 -0500

    Implementing nodemailer

---
+++ pages/api/contact.js
@@ -0,0 +1,58 @@
+import nodemailer from 'nodemailer';
+import { IncomingForm } from 'formidable';
+
+
+export const config = {
+    api: {
+        bodyParser: false, // Disabling body parsing
+    },
+};
+
+export default async (req, res) => {
+    if (req.method === 'POST') {
+        // Parse the form data
+        const form = new IncomingForm();
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                console.error('Error parsing the form data', err);
+                res.status(500).json({ success: false, message: 'Error parsing form data' });
+                return;
+            }
+
+            // Extract fields
+            const { name, email, phone, subject, message } = fields;
+
+            // Set up nodemailer transporter
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASSWORD,
+                },
+            });
+
+            // Set up email data
+            const mailOptions = {
+                from: process.env.EMAIL_FROM,
+                to: process.env.EMAIL_TO,
+                subject: `New Form Submission: ${subject}`,
+                text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+            };
+
+            // Send the email
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).json({ success: false, message: 'Error sending email' });
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    res.status(200).json({ success: true, message: 'Email sent successfully' });
+                }
+            });
+        });
+    } else {
+        // Handle any other HTTP method
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+};
--- pages/contactus.tsx
+++ pages/contactus.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, FormEvent, useRef } from "react";
+import { useRouter } from 'next/router';
 import { Card, Button, Input, Textarea, Select, SelectItem } from "@nextui-org/react";
 import DefaultLayout from "@/layouts/default";
 import { title } from "@/components/primitives";
@@ -12,6 +13,42 @@ export default function ContactUsPage() {
         { value: 'Feedback', label: 'Feedback' },
         { value: 'General Inquiry', label: 'General Inquiry' }
     ];
+
+    const [submissionSuccess, setSubmissionSuccess] = useState(false);
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const router = useRouter();
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+    
+        try {
+            const response = await fetch('/api/contact', {
+                method: 'POST',
+                body: formData,
+            });
+            const data = await response.json();
+    
+            if (data.success) {
+                setSuccessMessage('Thank you for your message. We will get back to you soon!');
+                setSubmissionSuccess(true);
+
+                // Check if the form is still mounted and then reset
+                if (formRef.current) {
+                    formRef.current.reset();
+                }
+            } else {
+                // Handle failure
+                console.error('Failed to send message:', data.message);
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    };
+    
+    
 
     return (
         <DefaultLayout>
@@ -27,28 +64,24 @@ export default function ContactUsPage() {
                 <Card className="bg-white/90 backdrop-blur-md border-4 border-green-800 w-full p-4 " isBlurred radius="lg">
                     <h2 className={`${title({ color: "green" })} font-semibold mb-4 text-center`}>Contact Us</h2>
                     
-                    <form 
-                        action="https://formspree.io/f/mrgnzyak" 
-                        method="POST" 
-                        className="flex flex-col gap-4"
-                    >
+                    <form ref={formRef} onSubmit={handleSubmit} className="flex flex-col gap-4">
                         <Input
                         isRequired
                         type="name"
-                        name="name"  // Add a name attribute
+                        name="name"
                         label="Name"
                         className="max-w-full border-2 border-green-800 rounded-lg"
                         />
                         <Input
                         isRequired
                         type="email"
-                        name="email"  // Add a name attribute
+                        name="email" 
                         label="Email"
                         className="max-w-full border-2 border-green-800 rounded-lg"
                         />
                         <Input
                         type="tel"
-                        name="phone"  // Add a name attribute
+                        name="phone" 
                         label="Phone Number"
                         className="max-w-full border-2 border-green-800 rounded-lg"
                         />
@@ -56,7 +89,7 @@ export default function ContactUsPage() {
                         {/* Dropdown for Subject */}
                         <Select
                         isRequired
-                        name="subject"  // Add a name attribute
+                        name="subject"
                         label="Subject"
                         className="max-w-full border-2 border-green-800 rounded-lg"
                         >
@@ -69,7 +102,7 @@ export default function ContactUsPage() {
 
                         <Textarea
                         isRequired
-                        name="message"  // Add a name attribute
+                        name="message"
                         label="Message"
                         placeholder="How can we help?"
                         className="max-w-full border-2 border-green-800 rounded-lg mb-[-7px]"
@@ -85,6 +118,12 @@ export default function ContactUsPage() {
                         </Button>
                     </form>
                 </Card>
+
+                {submissionSuccess && (
+                    <div className="text-center text-green-500 mt-4">
+                        {successMessage}
+                    </div>
+                )}
             </div>
         </DefaultLayout>
     );
