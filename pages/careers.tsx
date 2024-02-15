ce06e11cff0939252260e1dac17b15a5d6fd9eab
Seleem <sseleem1601@gmail.com>
2024-02-15 12:30:41 -0500

    Update hiring page

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -3,8 +3,8 @@ import { GetServerSideProps } from 'next';
 export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     redirect: {
-      destination: 'https://www.notion.so/potcast-careers/33bcf74995e242b3a2456b2f58f1a184?v=4832f153258e413bb5823583d9bca47d',
-      permanent: false, // set to true if this is a permanent redirect
+      destination: 'https://thepotcastproductions.notion.site/Hiring-3a9f64f8c693463ab4557287dd0d85b8?pvs=4',
+      permanent: true, // set to true if this is a permanent redirect
     },
   };
 };
